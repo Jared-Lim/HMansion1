ef962e3b0ed7d6a0a0851ada5c34e524d901df28
Jared <jaredlgw@outlook.com>
2016-11-15 21:24:19 +1100

    removed fluff code

--- src/heretic2/FolderParse.java
+++ src/heretic2/FolderParse.java
@@ -4,15 +4,7 @@ import java.io.File;
 
 public class FolderParse {
 	public static void main(String[] args) {
-		File directory = null;
-		
-		directory = new File("res/skills/");
-		
-		String a = directory.getAbsolutePath();
-		
-		System.out.println(a);
-		
-		System.out.println(directory.isDirectory());
+		File directory = new File("res/skills/");
 		String[] files = directory.list();
 		for(String f:files){
 			String g = "res/skills/"+f;
