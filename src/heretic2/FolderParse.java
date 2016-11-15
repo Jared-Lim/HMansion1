74e2b585177a9c6b5b2ba7516864f4dc3ad2dc7e
Jared <jaredlgw@outlook.com>
2016-11-15 23:35:38 +1100

    trying to insert everything now

--- src/heretic2/FolderParse.java
+++ src/heretic2/FolderParse.java
@@ -2,23 +2,36 @@ package heretic2;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import com.google.gson.Gson;
 
 public class FolderParse {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		Gson gson = new Gson();
 		File directory = new File("res/skills/");
 		String[] files = directory.list();
+		
+		Connection c = null;
+		Statement stmt = null;
+		
+		Class.forName("org.sqlite.JDBC");
+		c = DriverManager.getConnection("jdbc:sqlite:skills.db");
+		System.out.println("Opened database successfully");
+		stmt = c.createStatement();
 		
 		for(String f:files){
 			String g = "res/skills/"+f;
 			Reader reader = new FileReader(g);
 			Skill skill = gson.fromJson(reader, Skill.class);
 			reader.close();
-			System.out.println(skill.check());
+			
+			stmt.executeUpdate(skill.sql());
 		}
+		stmt.close();
+		c.close();
 	}
 }
