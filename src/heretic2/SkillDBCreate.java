209dc3791621ff92401de1bd389c3c7931d0d832
Jared <jaredlgw@outlook.com>
2016-11-15 23:12:48 +1100

    Renamed test file

--- src/heretic2/SkillDBCreate.java
+++ src/heretic2/SkillDBCreate.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class SkillDBTest {
+public class SkillDBCreate {
 	public static void main( String args[] )
 	{
 		Connection c = null;
