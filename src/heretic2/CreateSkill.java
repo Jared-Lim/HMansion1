3d6566a70b4b8091b6391ad40fdcaf100df1a076
Jared <jaredlgw@outlook.com>
2016-11-15 21:15:08 +1100

    Testing out skill, looks well

---
+++ src/heretic2/CreateSkill.java
@@ -0,0 +1,19 @@
+package heretic2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import com.google.gson.Gson;
+
+public class CreateSkill {
+	public static void main(String[] args) throws IOException {
+		Gson gson = new Gson();
+		Reader reader = new FileReader("res/skills/Titanomachia.json");
+		
+		Skill skill = gson.fromJson(reader, Skill.class);
+		reader.close();
+		System.out.println(skill.toString());
+		System.exit(0);
+	}
+}
