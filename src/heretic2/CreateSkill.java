d2c90a67c84cd61e2bf0f8cc4c454aff41f3c3f2
Jared <jaredlgw@outlook.com>
2016-11-15 23:21:04 +1100

    FIRST INSERT TEST

--- src/heretic2/CreateSkill.java
+++ src/heretic2/CreateSkill.java
@@ -3,6 +3,9 @@ package heretic2;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import com.google.gson.Gson;
 
@@ -14,6 +17,26 @@ public class CreateSkill {
 		Skill skill = gson.fromJson(reader, Skill.class);
 		reader.close();
 		System.out.println(skill.sql());
-		System.exit(0);
+		
+		Connection c = null;
+		Statement stmt = null;
+		try{
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:skills.db");
+			System.out.println("Opened database successfully");
+			
+			stmt = c.createStatement();
+			String sql = skill.sql();
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();	
+			
+		}catch(Exception e){
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+		System.out.println("Records created successfully");
+		
+		
 	}
 }
