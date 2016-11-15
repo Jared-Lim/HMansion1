957440f3f484d39f9c09275a41c46eea4f6d17d6
Jared <jaredlgw@outlook.com>
2016-11-16 01:08:46 +1100

    Got rid of the tries because i know what went wrong

--- src/heretic2/FolderParse.java
+++ src/heretic2/FolderParse.java
@@ -2,58 +2,36 @@ package heretic2;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.google.gson.Gson;
 
 public class FolderParse {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		Gson gson = new Gson();
 		File directory = new File("res/skills/");
 		String[] files = directory.list();
 		
 		Connection c = null;
 		Statement stmt = null;
-		try{
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:skills.db");
-		}catch (Exception e){
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-			System.exit(0);
-		}
-		System.out.println("Opened database successfully");
 		
-		try {
-			stmt = c.createStatement();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Class.forName("org.sqlite.JDBC");
+		c = DriverManager.getConnection("jdbc:sqlite:skills.db");
+		System.out.println("Opened database successfully");
+		stmt = c.createStatement();
 		
 		for(String f:files){
 			String g = "res/skills/"+f;
 			Reader reader = new FileReader(g);
 			Skill skill = gson.fromJson(reader, Skill.class);
 			reader.close();
-			try{
-				stmt.executeUpdate(skill.sql());
-				System.out.println("inserted "+skill.check());
-			}catch(Exception e){
-				System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-			    System.exit(0);
-			}
+			
+			stmt.executeUpdate(skill.sql());
 		}
-		try{
-			stmt.close();
-			c.close();
-		}catch(Exception e){
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		    System.exit(0);
-		}
+		stmt.close();
+		c.close();
 	}
 }
