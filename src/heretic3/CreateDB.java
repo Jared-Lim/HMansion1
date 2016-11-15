6ff1259b7068bc109380dfc250984f6361f0ae88
Jared <jaredlgw@outlook.com>
2016-11-16 03:47:54 +1100

    Looks quite neat

--- src/heretic3/CreateDB.java
+++ src/heretic3/CreateDB.java
@@ -8,16 +8,42 @@ public class CreateDB {
 	
 	public static void main(String[] args) {
 		try {
-			table();
+			makeSkillsTable();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
-	
-	public static void table() throws Exception{
-		Connection conn = null;
-		conn = DriverManager.getConnection(db);
-		System.out.println("Opened database successfully");
+	public static void makeSkillsTable() throws Exception{
+		Connection conn = null;		
+		PreparedStatement pstate = null;
+		String sql ="CREATE TABLE skills ("+
+					"nameJP STRING PRIMARY KEY,"+
+					"nameEN STRING,"+
+					"attr 	STRING,"+
+					"cost 	INT,"+
+					"power 	STRING,"+
+					"hits 	STRING,"+
+					"kuli 	STRING,"+
+					"hit 	STRING,"+
+					"targ 	STRING,"+
+					"str 	INT,"+
+					"effect STRING)";
+		try{
+			conn = DriverManager.getConnection(db);
+			System.out.println("Opened database successfully");
+			pstate = conn.prepareStatement(sql);
+			pstate.executeUpdate();
+			System.out.println("Created skills table");
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}finally{
+			if(pstate!=null){pstate.close();}
+			if(conn!=null){conn.close();}
+		}
 	}
+
+	
+
 }
