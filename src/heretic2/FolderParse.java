b250bc75cd1b6f365cbd2e031f443db41add2c61
Jared <jaredlgw@outlook.com>
2016-11-15 21:50:08 +1100

    moved gson out of loop

--- src/heretic2/FolderParse.java
+++ src/heretic2/FolderParse.java
@@ -9,11 +9,12 @@ import com.google.gson.Gson;
 
 public class FolderParse {
 	public static void main(String[] args) throws IOException {
+		Gson gson = new Gson();
 		File directory = new File("res/skills/");
 		String[] files = directory.list();
+		
 		for(String f:files){
 			String g = "res/skills/"+f;
-			Gson gson = new Gson();
 			Reader reader = new FileReader(g);
 			Skill skill = gson.fromJson(reader, Skill.class);
 			reader.close();
