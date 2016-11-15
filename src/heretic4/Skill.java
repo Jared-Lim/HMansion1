23d2e8a38f48834092e54a7229ff9cc91fc6acc1
Jared <jaredlgw@outlook.com>
2016-11-16 09:58:37 +1100

    Added insert function

--- src/heretic4/Skill.java
+++ src/heretic4/Skill.java
@@ -1,5 +1,7 @@
 package heretic4;
 
+import java.sql.*;
+
 public class Skill {
 	public String nameJP;
 	public String nameEN;
@@ -30,5 +32,27 @@ public class Skill {
 	
 	public String check(){
 		return nameEN+"-----"+nameJP;
+	}
+	
+	public void Insert(Connection conn, String db){
+		String sql = "INSERT INTO skills (nameJP,nameEN,attr,cost,power,hits,kuli,hit,targ,str,effect) "+
+				"VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+		try(PreparedStatement pstate = conn.prepareStatement(sql)){
+			pstate.setString(1, nameJP);
+			pstate.setString(2, nameEN);
+			pstate.setString(3, attribute);
+			pstate.setInt(4, cost);
+			pstate.setString(5, power);
+			pstate.setString(6, hits);
+			pstate.setString(7, kuli);
+			pstate.setString(8, hit);
+			pstate.setString(9, target);
+			pstate.setInt(10, strengthen);
+			pstate.setString(11, effect);
+			pstate.executeUpdate();
+			System.out.println("Inserted "+check());
+		}catch(SQLException e){
+			System.out.println(e.getMessage());;
+		}
 	}
 }
