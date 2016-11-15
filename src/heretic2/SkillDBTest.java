552639c110838ef6c9ffd062cb6892de4a872a88
Jared <jaredlgw@outlook.com>
2016-11-15 22:26:45 +1100

    FIRST TABLE ITERATION

--- src/heretic2/SkillDBTest.java
+++ src/heretic2/SkillDBTest.java
@@ -2,19 +2,31 @@ package heretic2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
-@SuppressWarnings("unused")
 public class SkillDBTest {
 	public static void main( String args[] )
 	{
 		Connection c = null;
+		Statement stmt = null;
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:skills.db");
+			System.out.println("Opened database successfully");
+			
+			stmt = c.createStatement();
+			String sql = 	"CREATE TABLE SKILLS "+
+						   "(nameJP STRING PRIMARY KEY,"+
+							"nameEN STRING,"+
+							"attr STRING)";
+			
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();			
 		} catch ( Exception e ) {
 			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 			System.exit(0);
 		}
-		System.out.println("Opened database successfully");
+	    System.out.println("Table created successfully");
 	}
 }
