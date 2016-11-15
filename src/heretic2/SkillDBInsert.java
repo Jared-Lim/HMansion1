74739092de420cb787227bc952d59a8d426c3a64
Jared <jaredlgw@outlook.com>
2016-11-15 22:39:56 +1100

    First iteration of insert testing

---
+++ src/heretic2/SkillDBInsert.java
@@ -0,0 +1,27 @@
+package heretic2;
+
+import java.sql.*;
+
+public class SkillDBInsert {
+	public static void main(String[] args) {
+		Connection c = null;
+		Statement stmt = null;
+		try{
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:skills.db");
+			System.out.println("Opened database successfully");
+			
+			stmt = c.createStatement();
+			String sql = 	"INSERT INTO SKILLS (nameJP,nameEN,attr) "+
+							"VALUES (ティタノマキア,Titanomachia,Physical);";
+			stmt.executeQuery(sql);
+			stmt.close();
+			c.close();	
+			
+		}catch(Exception e){
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+		System.out.println("Records created successfully");
+	}
+}
