54caf3584ca1b20442add3dd69819e2caec48c0a
Jared <jaredlgw@outlook.com>
2016-11-20 08:47:30 +1100

    Editted format

--- src/heretic1/Stats.java
+++ src/heretic1/Stats.java
@@ -12,9 +12,9 @@ public class Stats {
 	@Override
 	public String toString(){
 		
-		String output = String.format("====| hp  | mp  | str | skl | mag | spd | lck |\n"
-									+ "====-------------------------------------------\n"
-									+ "====| %3d | %3d | %3d | %3d | %3d | %3d | %3d |", hp, mp, strength, skill, magic, speed, luck);
+		String output = String.format("| hp  | mp  | str | skl | mag | spd | lck |\n"
+									+ "-------------------------------------------\n"
+									+ "| %3d | %3d | %3d | %3d | %3d | %3d | %3d |", hp, mp, strength, skill, magic, speed, luck);
 		return output;
 	}
 
