fc44b1dde843d92f458d47599c6325113aaa9f9a
Jared <jaredlgw@outlook.com>
2016-11-20 08:47:52 +1100

    Format pt2

--- src/heretic1/Stats.java
+++ src/heretic1/Stats.java
@@ -14,7 +14,8 @@ public class Stats {
 		
 		String output = String.format("| hp  | mp  | str | skl | mag | spd | lck |\n"
 									+ "-------------------------------------------\n"
-									+ "| %3d | %3d | %3d | %3d | %3d | %3d | %3d |", hp, mp, strength, skill, magic, speed, luck);
+									+ "| %3d | %3d | %3d | %3d | %3d | %3d | %3d |", 
+									hp, mp, strength, skill, magic, speed, luck);
 		return output;
 	}
 
